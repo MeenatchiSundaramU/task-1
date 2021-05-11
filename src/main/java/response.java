fbf91194ab174ecec74dc833c69d421e4c2bd72b
MeenatchiSundaramU <69109473+MeenatchiSundaramU@users.noreply.github.com>
2021-05-11 18:37:19 +0530

    FinalsCommited

--- src/main/java/response.java
+++ src/main/java/response.java
@@ -31,18 +31,21 @@ public class response extends HttpServlet
 		{
 		String code=req.getParameter("code");
 		String grand_token_api="https://accounts.zoho.com/oauth/v2/token?grant_type=authorization_code&client_id=1000.CU8EIFGUKKWO76XF6DJI5PMJ072P2T&client_secret=1a69af9c58c99acb88dd7de3be5698fe9de8cfd01a&redirect_uri=http://localhost:8080/final/resp&code="+req.getParameter("code");
-		String grand_token=callFun(grand_token_api);
+		String grand_token=callFun(grand_token_api,0,"");
 		 int ind1=grand_token.indexOf(":");
 	     int ind2=grand_token.indexOf(",");
-	     String access_token=callFun("https://accounts.zoho.com/oauth/v2/token/revoke?token="+grand_token.substring(ind1+2,ind2-1));
+	     String grand_token_value=grand_token.substring(ind1+2,ind2-1);
+	     String users=callFun("https://accounts.zoho.com/oauth/user/info",1,grand_token_value);//Fetch the user details
+			try {
+				ExtractUserProfile(users,grand_token_value);//extracts and upload to database
+			} catch (ClassNotFoundException | SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	     String access_token=callFun("https://accounts.zoho.com/oauth/v2/token/revoke?token="+grand_token.substring(ind1+2,ind2-1));//Revoke Functionality to expires the functionality of token
 	     int finind1=access_token.toString().indexOf(":")+2;
 	     int finind2=access_token.toString().lastIndexOf('"');
 	     if(access_token.toString().substring(finind1, finind2).equals("success")) {
-	    	 try {
-					uploadDatabase(access_token,req.getParameter(username).toString());
-					} catch (ClassNotFoundException | SQLException e) {
-						e.printStackTrace();
-				 }
 	        	resp.sendRedirect("http://localhost:8080/final/resp.html");
 	             }
 	     else
@@ -52,10 +55,15 @@ public class response extends HttpServlet
 	     
          }
 	}
-	String callFun(String url) throws IOException
+	String callFun(String url,int mode,String grandtok) throws IOException
 	{
 		URL obj= new URL(url);
 	    HttpsURLConnection con=(HttpsURLConnection)obj.openConnection();
+	    if(mode==1)
+	    {
+	    	con.setRequestProperty("Authorization","Bearer "+" "+grandtok);
+	        con.setRequestProperty("Content-Type","application/json");
+	    }
 	    con.setRequestMethod("POST");
 	    int respcode=con.getResponseCode();
 		 BufferedReader in = new BufferedReader(
@@ -68,19 +76,28 @@ public class response extends HttpServlet
 	     in.close();
 	     return response.toString();
 	}
-	void uploadDatabase(String persistent_tokens,String name) throws ClassNotFoundException, SQLException
+	void ExtractUserProfile(String userjson,String grand_token_value) throws ClassNotFoundException, SQLException
+	{
+		String username=userjson.substring((userjson.indexOf("First_name")+16),(userjson.indexOf(",")-1));
+		String email=userjson.substring((userjson.indexOf("Email")+8),(userjson.indexOf(",",(userjson.indexOf("Email")+7))-1));
+		uploadDatabase(grand_token_value,username,email);
+		
+	}
+	void uploadDatabase(String persistent_tokens,String name,String email) throws ClassNotFoundException, SQLException
 	{
 		 Connection c = null;
-                 Statement stmt = null;
+                 PreparedStatement stmt = null;
 		 try {
          Class.forName("org.sqlite.JDBC");
          c = DriverManager.getConnection("jdbc:sqlite:C://sqlite-tools-win32-x86-3350500//loginusers");
          c.setAutoCommit(false);
          System.out.println("database Opened");
-	 stmt = c.createStatement();
-         String sql = "INSERT INTO USERS(NAME,ACCESSTOKEN) VALUES("+name+","+persistent_tokens+");";//PRIMARY KEY---->ID AUTO GENERATED;
-         stmt.executeUpdate(sql);
-	 stmt.close();
+	     stmt = c.prepareStatement("insert into users(name,email,persistenttokens)values(?,?,?)");//PRIMARY KEY---->ID AUTO GENERATED;
+	     stmt.setString(1,name);
+	     stmt.setString(2,email);
+	     stmt.setString(3,persistent_tokens);
+         stmt.executeUpdate();
+	     stmt.close();
          c.commit();
          c.close();
       } catch ( Exception e ) {
@@ -89,6 +106,5 @@ public class response extends HttpServlet
       }
       System.out.println("USERS ADDED");
    }
-	}
 }
 
